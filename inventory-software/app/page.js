1a5757f0f342448a107d9714dbe64e7bdf903ab3
deb-seop <77163842+seop-kim@users.noreply.github.com>
2025-06-23 20:23:43 +0900

    won : created project

---
+++ inventory-software/app/page.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className='flex items-center justify-center min-h-screen'>
+      <h2 className="text-3xl">
+        Inventory Management Software
+      </h2>
+    </div>
+  );
+}
